90afc158ef6b38722a3c926b0c1f9c69f6f5c465
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-12-29 12:35:02 -0500

    only need object (contracts and inbox) from compiled Smart contract

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // To get the path on any system 
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1)); // compiles the solidity source code 
+module.exports = solc.compile(source, 1).contracts[':Inbox']; // compiles the solidity source code 
