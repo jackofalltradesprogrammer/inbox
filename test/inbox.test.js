6b0671e5976ab4295ebe7e682718f24d0f8c7dce
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-12-29 16:02:12 -0500

    mocha testing with it,describe, beforeEach

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,3 +2,27 @@ const assert = require('assert'); // use for assertions
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // Web3 is camel case because it is a constructor
 const web3 = new Web3(ganache.provider())// working with an instance
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+let car;
+beforeEach(() => {
+    car = new Car();
+});
+
+describe('Car', () => {
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+});
