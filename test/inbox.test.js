a036eb785f839493d32ea49f0b70dc6f7cddc83c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-12-29 16:20:14 -0500

    fetch ganache accounts

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,26 +3,50 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // Web3 is camel case because it is a constructor
 const web3 = new Web3(ganache.provider())// working with an instance
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
 beforeEach(() => {
-    car = new Car();
+    // Get a list of all accounts
+    web3.eth.getAccounts()   // every function is async returns a promise
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+    // Use one of those accounts to deploy
+
+    // the contract
 });
 
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
     });
 });
+
+
+
+
+
+
+// MOCHA TESTING DEMO
+
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+// beforeEach(() => {
+//     car = new Car();
+// });
+
+// describe('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
